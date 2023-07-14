62383d76c37510f6f0d0ae7faf30b715015efa05
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-07-14 16:17:21 +0300

    firstTask

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createGallery(galleryItems) {
+  const makeLiItem = galleryItems.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+          </a>
+        </li>`)
+    .join('');
+
+  galleryList.insertAdjacentHTML('afterbegin', makeLiItem);
+}
+createGallery(galleryItems);
+// openModalWindow
+galleryList.addEventListener('click', openGalleryImg);
+
+
+
+function openGalleryImg(event) {
+    event.preventDefault();
+    const clickImg = event.target.classList.contains('gallery__image');
+        if (!clickImg){
+          return;  
+        };
+        
+        if (event.target) {
+            createImg();
+        };       
+    };
+    
+function createImg() {
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="900" height="900" >`);
+    instance.show();
+
+    document.addEventListener('keydown', closeGalleryImg);
+};
+
+// closeModalWindow
+function closeGalleryImg(event) {
+    const escKey = 'Escape';
+    if (event.code === escKey) {
+      deleteModalImg();
+  
+      document.removeEventListener('keydown', closeGalleryImg);
+    }
+  };
+
+  function deleteModalImg() {
+    const instance = document.querySelector('.basicLightbox--visible');
+    instance.remove();
+  };
